704ffe4d688c5e0ab8fb2b3e3759f21862aa6883
kyoun <yngrk.kim@gmail.com>
2023-01-02 20:01:38 +0100

    added initial files

---
+++ script.js
@@ -0,0 +1,12 @@
+// const progressBar = document.querySelector(".progress-bar");
+// const progressText = document.querySelector(".progress-text");
+// const mainContent = document.querySelector("main");
+// const navItems = document.querySelectorAll(".nav-item");
+
+// mainContent.addEventListener("scroll", () => {
+//     let scrollPos = mainContent.scrollTop;
+//     let scrollMax = mainContent.scrollHeight - mainContent.offsetHeight;
+//     let percentage = scrollPos / scrollMax;
+//     progressBar.style["width"] = `${percentage * 100}%`;
+//     progressText.textContent = `${percentage}`;
+// });
